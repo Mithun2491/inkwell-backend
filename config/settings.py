9ad82a91057c8895f4b56228164f986dcedffbe5
mithun <mithunjerry24@gmail.com>
2026-06-21 19:41:56 +0530

    fix: replace deprecated STATICFILES_STORAGE with STORAGES dict for Django 5

--- config/settings.py
+++ config/settings.py
@@ -229,6 +229,9 @@ MEDIA_ROOT = BASE_DIR / "media"
 
 # Django 5 WhiteNoise
 STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
     "staticfiles": {
         "BACKEND":
             "whitenoise.storage."
